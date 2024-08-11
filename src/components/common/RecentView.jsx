bc4a9b09df5ad291909fe828610bbeb8e736b77d
anujYadav <anujvns28@gmail.com>
2024-08-11 12:31:47 +0530

    error resoleve

--- src/components/common/RecentView.jsx
+++ src/components/common/RecentView.jsx
@@ -1,49 +1,53 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import ProductCard from './ProductCard';
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/autoplay';
-import useGetViewPort from '../../hook/useGetViewPort';
+    import React from 'react'
+    import { useDispatch, useSelector } from 'react-redux'
+    import ProductCard from './ProductCard';
+    import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
+    import { Swiper, SwiperSlide } from 'swiper/react';
+    import 'swiper/css';
+    import 'swiper/css/navigation';
+    import 'swiper/css/pagination';
+    import 'swiper/css/autoplay';
+    import useGetViewPort from '../../hook/useGetViewPort';
 
-const RecentView = () => {
-    const {recentlyView} = useSelector((state) => state.profile);
-    const viewport = useGetViewPort();
-   
+    const RecentView = () => {
+        const {recentlyView} = useSelector((state) => state.profile);
+        const viewport = useGetViewPort();
+      
 
-  return (
-    <div>
-        <p className='text-xl font-semibold italic text-blue-500 my-3'>Recent</p>
-        {
+      return (
+        <div>
+          {
             recentlyView.length !== 0 && 
-            <div  className='flex flex-row flex-wrap w-full lg:gap-5 gap-2 p-1'>
+            <div>
+              <p className='text-xl font-semibold italic text-blue-500 my-3'>Recent</p>
+
+           <div  className='flex flex-row flex-wrap w-full lg:gap-5 gap-2 p-1'>
 
 
-                <Swiper
-                  modules={[Navigation, Pagination, Scrollbar, Autoplay]}
-                  navigation
-                  spaceBetween={10}
-                  slidesPerView={ viewport < 500 ? 2.2 : 4}
-                >
-                  {
-                    recentlyView
-                    .map((item,index) => (
-                      <div key={index}>
-                        <SwiperSlide>
-                        <ProductCard key={index} product={item}/>
-                        </SwiperSlide>
-                      </div>
-                    ))
-                  }
-                </Swiper>
-                
-            </div>
-        }
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar, Autoplay]}
+      navigation
+      spaceBetween={10}
+      slidesPerView={ viewport < 500 ? 2.2 : 4}
+    >
+      {
+        recentlyView
+        .map((item,index) => (
+          <div key={index}>
+            <SwiperSlide>
+            <ProductCard key={index} product={item}/>
+            </SwiperSlide>
+          </div>
+        ))
+      }ā
+    </Swiper>
+
     </div>
-  )
-}
+            </div>
+          }
+              
+        </div>
+      )
+    }
 
-export default RecentView
+    export default RecentView
